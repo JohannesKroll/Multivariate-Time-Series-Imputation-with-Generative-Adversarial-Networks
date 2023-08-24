1033115b7ad8fe71e4f208eec78fcbdf35bce057
Jockel <johannes.kroll@westend61.de>
2023-08-24 10:48:52 +0200

    start with company dataset

--- Gan_Imputation/Physionet_main.py
+++ Gan_Imputation/Physionet_main.py
@@ -7,8 +7,9 @@ Created on Fri Feb  2 13:54:53 2018
 """
 from __future__ import print_function
 import sys
+
 sys.path.append("..")
-import WGAN_GRUI 
+import WGAN_GRUI
 import tensorflow as tf
 import argparse
 import numpy as np
@@ -16,10 +17,12 @@ from Physionet2012Data import readData, readTestData
 import os
 
 """main"""
+
+
 def main():
     # parse arguments
     parser = argparse.ArgumentParser(description='manual to this script')
-    parser.add_argument('--gpus', type=str, default = None)
+    parser.add_argument('--gpus', type=str, default=None)
     parser.add_argument('--batch-size', type=int, default=128)
     parser.add_argument('--gen-length', type=int, default=96)
     parser.add_argument('--impute-iter', type=int, default=400)
@@ -29,10 +32,10 @@ def main():
     parser.add_argument('--model-path', type=str, default=None)
     parser.add_argument('--result-path', type=str, default=None)
     parser.add_argument('--dataset-name', type=str, default=None)
-    parser.add_argument('--g-loss-lambda',type=float,default=0.1)
-    parser.add_argument('--beta1',type=float,default=0.5)
+    parser.add_argument('--g-loss-lambda', type=float, default=0.1)
+    parser.add_argument('--beta1', type=float, default=0.5)
     parser.add_argument('--lr', type=float, default=0.001)
-    #lr 0.001的时候 pretrain_loss降的很快，4个epoch就行了
+    # lr 0.001的时候 pretrain_loss降的很快，4个epoch就行了
     parser.add_argument('--epoch', type=int, default=30)
     parser.add_argument('--n-inputs', type=int, default=41)
     parser.add_argument('--n-hidden-units', type=int, default=64)
@@ -44,65 +47,77 @@ def main():
                         help='Directory name to save the generated images')
     parser.add_argument('--log-dir', type=str, default='logs',
                         help='Directory name to save training logs')
-    parser.add_argument('--isNormal',type=int,default=1)
-    #0 false 1 true
-    parser.add_argument('--isBatch-normal',type=int,default=1)
-    parser.add_argument('--isSlicing',type=int,default=1)
-    parser.add_argument('--disc-iters',type=int,default=8)
+    parser.add_argument('--isNormal', type=int, default=1)
+    # 0 false 1 true
+    parser.add_argument('--isBatch-normal', type=int, default=1)
+    parser.add_argument('--isSlicing', type=int, default=1)
+    parser.add_argument('--disc-iters', type=int, default=8)
+    parser.add_argument('--csv-file', type=int, default=0)
     args = parser.parse_args()
-    
-    if args.isBatch_normal==0:
-            args.isBatch_normal=False
-    if args.isBatch_normal==1:
-            args.isBatch_normal=True
-    if args.isNormal==0:
-            args.isNormal=False
-    if args.isNormal==1:
-            args.isNormal=True
-    if args.isSlicing==0:
-            args.isSlicing=False
-    if args.isSlicing==1:
-            args.isSlicing=True
 
-    #make the max step length of two datasett the same
-    epochs=[30]
-    g_loss_lambdas=[0.15]
-    beta1s=[0.5]
+    if args.isBatch_normal == 0:
+        args.isBatch_normal = False
+    if args.isBatch_normal == 1:
+        args.isBatch_normal = True
+    if args.isNormal == 0:
+        args.isNormal = False
+    if args.isNormal == 1:
+        args.isNormal = True
+    if args.isSlicing == 0:
+        args.isSlicing = False
+    if args.isSlicing == 1:
+        args.isSlicing = True
+    if args.csv_file == 0:
+        args.csv_file = False
+    if args.csv_file == 1:
+        args.csv_file = True
+
+    # make the max step length of two datasett the same
+    epochs = [30]
+    g_loss_lambdas = [0.15]
+    beta1s = [0.5]
     for beta1 in beta1s:
         for e in epochs:
             for g_l in g_loss_lambdas:
-                args.epoch=e
-                args.beta1=beta1
-                args.g_loss_lambda=g_l
+                args.epoch = e
+                args.beta1 = beta1
+                args.g_loss_lambda = g_l
                 tf.reset_default_graph()
-                dt_train=readData.ReadPhysionetData(os.path.join(args.data_path,"train"), os.path.join(args.data_path,"train","list.txt"),isNormal=args.isNormal,isSlicing=args.isSlicing)
-                dt_test=readTestData.ReadPhysionetData(os.path.join(args.data_path,"test"), os.path.join(args.data_path,"test","list.txt"),dt_train.maxLength,isNormal=args.isNormal,isSlicing=args.isSlicing)
+                dt_train = readData.ReadPhysionetData(args.data_path, os.path.join(args.data_path, "train", "list.txt"),
+                                                      isNormal=args.isNormal, isSlicing=args.isSlicing,
+                                                      readCsv=args.csv_file)
+                dt_test = readTestData.ReadPhysionetData(args.data_path,
+                                                         os.path.join(args.data_path, "test", "list.txt"),
+                                                         dt_train.maxLength, isNormal=args.isNormal,
+                                                         isSlicing=args.isSlicing, readCsv=args.csv_file)
                 tf.reset_default_graph()
-                config = tf.ConfigProto() 
-                config.gpu_options.allow_growth = True 
+                config = tf.ConfigProto()
+                config.gpu_options.allow_growth = True
                 with tf.Session(config=config) as sess:
                     gan = WGAN_GRUI.WGAN(sess,
-                                args=args,
-                                datasets=dt_train,
-                                )
-            
+                                         args=args,
+                                         datasets=dt_train,
+                                         )
+
                     # build graph
                     gan.build_model()
-            
+
                     # show network architecture
-                    #show_all_variables()
-            
+                    # show_all_variables()
+
                     # launch the graph in a session
                     gan.train()
                     print(" [*] Training finished!")
-                    
-                    gan.imputation(dt_train,True)
-                    
+
+                    gan.imputation(dt_train, True)
+
                     print(" [*] Train dataset Imputation finished!")
-                    
-                    gan.imputation(dt_test,False)
-                    
+
+                    gan.imputation(dt_test, False)
+
                     print(" [*] Test dataset Imputation finished!")
                 tf.reset_default_graph()
+
+
 if __name__ == '__main__':
     main()
